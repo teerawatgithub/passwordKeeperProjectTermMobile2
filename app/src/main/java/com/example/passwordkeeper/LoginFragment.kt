35f99f9c713dd3213de749b3a873857a9316e279
teerawatgithub <59160069@go.buu.ac.th>
2019-10-15 19:37:47 +0700

    initail and create login fragment compleded

---
+++ app/src/main/java/com/example/passwordkeeper/LoginFragment.kt
@@ -0,0 +1,32 @@
+package com.example.passwordkeeper
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.passwordkeeper.databinding.FragmentLoginBinding
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater,
+            R.layout.fragment_login,container,false)
+        return binding.root
+    }
+
+
+}
