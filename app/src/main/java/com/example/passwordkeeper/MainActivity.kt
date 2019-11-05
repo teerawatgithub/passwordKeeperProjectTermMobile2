8a82d175dcdf4627674aa2756b1417d6a2d22752
teerawatgithub <59160069@go.buu.ac.th>
2019-11-06 00:32:04 +0700

    loggin add activity lifecycle

--- app/src/main/java/com/example/passwordkeeper/MainActivity.kt
+++ app/src/main/java/com/example/passwordkeeper/MainActivity.kt
@@ -3,12 +3,44 @@ package com.example.passwordkeeper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("MainActivity", "onCreate Called")
+        Timber.i("onCreate called")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Timber.i("onStart Called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.i("onResume Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.i("onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Timber.i("onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.i("onDestroy Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Timber.i("onRestart Called")
+    }
+
 }
