b0d03c6167ba7e7347e992bfebf955b9ae46cd60
teerawatgithub <59160069@go.buu.ac.th>
2019-11-06 16:26:44 +0700

    add about complete

--- app/src/main/java/com/example/passwordkeeper/VaultFragment.kt
+++ app/src/main/java/com/example/passwordkeeper/VaultFragment.kt
@@ -3,12 +3,12 @@ package com.example.passwordkeeper
 
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.passwordkeeper.databinding.FragmentLoginBinding
 import com.example.passwordkeeper.databinding.FragmentVaultBinding
 
@@ -29,7 +29,20 @@ class VaultFragment : Fragment() {
         binding.addButton.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_vaultFragment_to_addPasswordFragment)
         }
+        setHasOptionsMenu(true)
         return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,
+            view!!.findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+
 
 }
